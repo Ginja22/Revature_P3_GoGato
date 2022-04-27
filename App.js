be885877b79e473db297f8efcd3f91e58b50723d
Ginja22 <88261803+Ginja22@users.noreply.github.com>
2022-04-27 14:52:58 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+// Author: Marcus & Elaine
+
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App"></div>
+      <div className="btn1">
+        <button>Hello!</button>
+      </div>
+      <div className="btn2">
+        <button>Hello!</button>
+      </div>
+      <div className="btn3">
+        <button>Hello!</button>
+      </div>
+      <div className="btn4">
+        <button>Hello!</button>
+      </div>
+      <div className="btn5">
+        <button>Hello!</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
